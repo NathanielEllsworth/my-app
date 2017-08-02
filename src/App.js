9ff10e774d4d2fd82cf5323e08b57b3b45f8a394
Nathaniel Ellsworth <NathanielEllsworth@gmail.com>
2017-08-02 09:02:09 -0400

    pushing dummy data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, {Component} from 'react';
 import './App.css';
 
+const  list = [
+    {
+        title: 'React',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url:'https://github.com/reactjs/redux',
+        author:'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        pojectID: 1,
+    },
+];
+
 class App extends Component {
     render() {
-        const helloWorld = 'Welcome to the Road to learn React test';
         return (
             <div className="App">
-                <h2>{helloWorld}</h2>
-            </div>
-
-        );
-    }
-}
-
+                { list.map(function(item) {
+                    return <div>{item.title}</div>;
+                })}
+            </div> );
+    } }
 export default App;
