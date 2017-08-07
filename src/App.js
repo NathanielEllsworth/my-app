b5dc2ae7a6034f4fb3c09eb0de014eab4a81ba98
Nathaniel Ellsworth <NathanielEllsworth@gmail.com>
2017-08-07 15:31:02 -0400

    Split up components

--- src/App.js
+++ src/App.js
@@ -68,6 +68,21 @@ class App extends Component {
         const {searchTerm, list} = this.state;
         return (
             <div className="App">
+
+
+                <Search
+                    value={searchTerm}
+                    onChange={this.onSearchChange}
+                />
+
+
+                <Table
+                    list={list}
+                    pattern={searchTerm}
+                    onDismiss={this.onDismiss}
+                />
+
+
                 <form>
                     <input
                         type="text"
@@ -75,6 +90,8 @@ class App extends Component {
                         onChange={this.onSearchChange}
                     />
                 </form>
+
+
                 {list.filter(isSearched(searchTerm)).map(item =>
 
                     <div key={item.objectID}>
@@ -93,6 +110,49 @@ class App extends Component {
                             </button>
                         </span>
                     </div>
+                )}
+            </div>
+        );
+    }
+}
+
+class Search extends Component {
+    render() {
+        const { value, onChange } = this.props;
+        return (
+            <form>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                />
+            </form>
+        );
+    }
+}
+
+class Table extends Component {
+    render() {
+        const { list, pattern, onDismiss } = this.props;
+        return (
+            <div>
+                { list.filter(isSearched(pattern)).map(item =>
+                <div key={item.objectID}>
+                    <span>
+                        <a href={item.url}>{item.title}</a>
+                    </span>
+                    <span>{item.author}</span>
+                    <span>{item.num_comments}</span>
+                    <span>{item.points}</span>
+                    <span>
+                        <button
+                            onClick={() => onDismiss(item.objectID)}
+                            type="button"
+                        >
+                          Dismiss
+                        </button>
+                    </span>
+                </div>
                 )}
             </div>
         );
