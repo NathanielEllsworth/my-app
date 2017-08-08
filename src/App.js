88a232a7d577058c2ff51b4dc9b21834b30c8f5d
Nathaniel Ellsworth <NathanielEllsworth@gmail.com>
2017-08-08 11:24:35 -0400

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -78,7 +78,6 @@ class App extends Component {
 
     render() {
         const {searchTerm, result} = this.state;
-        if (!result){return null;}
         return (
             <div className="page">
                 <div className="interactions">
@@ -90,13 +89,13 @@ class App extends Component {
                     Search
                 </Search>
                 </div>
-
-                <Table
-                    result={result.hits}
-                    pattern={searchTerm}
-                    onDismiss={this.onDismiss}
-                />
-
+                { result &&
+                     <Table
+                        result={result.hits}
+                        pattern={searchTerm}
+                        onDismiss={this.onDismiss}
+                    />
+                }
             </div>
         );
     }
